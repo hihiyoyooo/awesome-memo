67894e6b38eb3f2ffdb92feb67dc861dd20b7140
hihiyoyooo <rlagusxo7979@naver.com>
2023-07-25 23:55:01 +0900

    [memo] 메모 수정 및 삭제 기능 추가
    js - 메모 수정 및 삭제 기능 추가
    서버와 연결 완료

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 from fastapi.staticfiles import StaticFiles
 
 class Memo(BaseModel):
-    id:int
+    id:str
     content:str
     
 memos = []
@@ -18,5 +18,21 @@ def create_memo(memo:Memo):
 @app.get("/memos")
 def read_memo():
     return memos
+
+@app.put("/memos/{memo_id}")
+def put_memo(req_memo:Memo):
+    for memo in memos:
+        if memo.id==req_memo.id:
+            memo.content=req_memo.content
+            return '성공했습니다.' 
+    return '그런 메모는 없습니다.'
+
+@app.delete("/memos/{memo_id}")
+def delete_memo(memo_id):
+    for index, memo in enumerate(memos):
+        if memo.id==memo_id:
+            memos.pop(index)
+            return '성공했습니다.'
+    return '그런 메모는 없습니다.'
 
 app.mount("/", StaticFiles(directory="static",html=True), name="static")
